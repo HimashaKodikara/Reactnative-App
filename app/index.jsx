969e603869dc9f0d1dd04362aae593b0713037f3
19840769 <kodikarah.21@uom.lk>
2024-06-26 15:41:18 +0530

    chore: Add nativewind babel plugin and update dependencies

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {Link} from 'expo-router';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Aur!</Text>
       <StatusBar style="auto" />
+      <Link href="/Profile" style={{color:'blue'}}>Go to Profile</Link>
     </View>
   );
 }
