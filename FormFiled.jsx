97618dd26826f433fa66efe67194e61f32a762dc
19840769 <kodikarah.21@uom.lk>
2024-07-09 20:08:45 +0530

    refactor: Improve FormFiled component styling and add support for password visibility toggle

--- FormFiled.jsx
+++ FormFiled.jsx
@@ -1,12 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity,Image } from 'react-native';
+import React, { useState } from 'react';
 
+import {icons} from './constants'
 const FormFiled = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
+  const [showpassword, setShowPassword] = useState(false)
   return (
     <View style={[styles.container, otherStyles]}>
       <Text style={styles.title}>{title}</Text>
       <View style={styles.inputContainer}>
-        {/* You might want to include a TextInput component here */}
+        <TextInput
+          style={styles.input}
+          value={value}
+          placeholder = {placeholder}
+          placeholderTextColor="#7b7b8b"
+          onChangeText={handleChangeText}
+          secureTextEntry ={title === 'Password' && !showpassword }
+        />
+        {title === 'Password' && (
+          <TouchableOpacity onPress={() => setShowPassword(!showpassword)}>
+            <Image source={!showpassword ? icons.eye : icons.eyeHide} style={{width:6, height:6,resizeMode:'contain'}}/>
+            </TouchableOpacity>
+        )}
       </View>
     </View>
   );
@@ -14,21 +28,27 @@ const FormFiled = ({ title, value, placeholder, handleChangeText, otherStyles, .
 
 const styles = StyleSheet.create({
   container: {
-    // Styles for the container
+    spaceBetween: 16,
   },
   title: {
-    color: "gray",
+    marginTop:20,
+    color: "white",
   },
   inputContainer: {
     borderWidth: 2,
     borderColor: '#BDBDBD', // Assuming this is the color you want for the border
-    width: '100%',
-    height: 64, // 16 * 4 to convert h-16
+    width: 300,
+    height: 50, // 16 * 4 to convert h-16
     paddingHorizontal: 16, // px-4
     backgroundColor: '#F5F5F5', // Assuming black-100 is a light shade
     borderRadius: 20, // rounded-2xl
     justifyContent: 'center',
   },
+  input:{
+    flex: 1,
+    color:'white'
+  
+  }
 });
 
 export default FormFiled;
