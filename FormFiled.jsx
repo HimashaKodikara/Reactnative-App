2481faab48883796dad8cc6079a97053565f8515
19840769 <kodikarah.21@uom.lk>
2024-07-09 19:47:27 +0530

    refactor: Improve FormFiled component styling and add support for additional props

--- FormFiled.jsx
+++ FormFiled.jsx
@@ -1,14 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
-const FormFiled = ({title, value, placeholder,handleChangeText,otherStyles,...props}) => {
+const FormFiled = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
   return (
-    <View style={ ` ${otherStyles}`}>
-      <Text style={{color:"gray",}}>{title}</Text>
-      <View style={{ borderColor:'red', borderRadius:5, padding:5,backgroundColor:'black',alignItems:'center',height:20, paddingHorizontal:4,width:100}}>
-        </View>
+    <View style={[styles.container, otherStyles]}>
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.inputContainer}>
+        {/* You might want to include a TextInput component here */}
+      </View>
     </View>
-  )
+  );
 }
 
-export default FormFiled
+const styles = StyleSheet.create({
+  container: {
+    // Styles for the container
+  },
+  title: {
+    color: "gray",
+  },
+  inputContainer: {
+    borderWidth: 2,
+    borderColor: '#BDBDBD', // Assuming this is the color you want for the border
+    width: '100%',
+    height: 64, // 16 * 4 to convert h-16
+    paddingHorizontal: 16, // px-4
+    backgroundColor: '#F5F5F5', // Assuming black-100 is a light shade
+    borderRadius: 20, // rounded-2xl
+    justifyContent: 'center',
+  },
+});
+
+export default FormFiled;
