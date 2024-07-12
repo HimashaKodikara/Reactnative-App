d34a793e92192cbfaab864e8b21c85210b120aa7
19840769 <kodikarah.21@uom.lk>
2024-07-12 22:09:57 +0530

    refactor: Update bookmark and home components

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Bookmark = () => {
   return (
     <View>
-      <Text>Bookmark</Text>
+      <Text style={{fontSize:30}}>Bookmark</Text>
     </View>
   )
 }
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Text style={{ fontSize: 20 }}>{item.id} </Text>
+        )}
+        ListHeaderComponent={() => (
+          <View style={styles.headerContainer}>
+            <View style={styles.headerContent}>
+              <View>
+                <Text style={styles.welcomeText}>Welcome Back</Text>
+                <Text style={styles.usernameText}>JdMastery</Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Home
+const styles = StyleSheet.create({
+  headerContainer: {
+    marginVertical: 6,
+    paddingHorizontal: 4,
+    height: 24,
+    
+  },
+  headerContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: 24,
+  },
+  welcomeText: {
+    fontSize: 50,
+    color: 'gray',
+  },
+  usernameText: {
+    fontSize: 20,
+  },
+});
+
+export default Home;
