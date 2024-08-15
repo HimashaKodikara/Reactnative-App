65eac88a5de71bc2e1a56297b6bf7fca117099ae
19840769 <kodikarah.21@uom.lk>
2024-08-15 23:32:47 +0530

    refactor: Update EmptyState component to remove unnecessary styles

--- EmptyState.jsx
+++ EmptyState.jsx
@@ -6,7 +6,7 @@ import { images } from './constants';
 const EmptyState = () => {
   return (
     <View style={{justifyContent:'center' ,alignItems:'center', paddingHorizontal:4}}>
-      <Image source ={images.empty} style={{width:'100px',height:"50px", resizeMode:'contain'}}/>
+      <Image source ={images.empty} style={{paddingHorizontal:20}}/>
       <Text style={{color:'gray'}}>Welcome Back</Text>
                 <Text style={{color:'white'}}>JdMastery</Text>
     </View>
