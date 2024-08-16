499863011c3e684477bfb8c9fc610d74da6a98d5
19840769 <kodikarah.21@uom.lk>
2024-08-16 22:30:27 +0530

    refactor: Remove unused useState import in home.jsx

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,5 @@
-import { View, Text, FlatList, StyleSheet, Image, RefreshControl,useState } from 'react-native';
-import React from 'react';
+import { View, Text, FlatList, StyleSheet, Image, RefreshControl } from 'react-native';
+import React,{useState} from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 import SearchInput from '../Searchinput';
