ce1f4b0355e261043eaa781d8d3f076c81a6bdd0
19840769 <kodikarah.21@uom.lk>
2024-08-27 20:02:34 +0530

    updated

--- app/Trending.jsx
+++ app/Trending.jsx
@@ -1,5 +1,5 @@
-import { View, Text, FlatList, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native'
+import React, { useState } from 'react';
 import * as Animatable from 'react-native-animatable';
 
 const zoomIn = {
@@ -25,11 +25,29 @@ const TrendingItem = ({activeItem,item}) =>{
   return(
     <Animatable.View
      style={{marginRight:5}}
-     animation={activeItem === item.$id ? zoomIn : zoomOut
-
-     }
+     animation={activeItem === item.$id ? zoomIn : zoomOut}
+     duration ={500}
     >
-
+   {
+    play ? (
+      <Text style={{color:'white'}}>Playing</Text>
+    ):(
+   <TouchableOpacity
+   style={{ flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'}}
+   activeOpacity={0.7}
+   onPress={() => setPlay(true)}>
+  <ImageBackground
+            source={{
+              uri: item.thumbnail,
+            }}
+            style={styles.ImageBack}
+            resizeMode="cover"
+          />
+   </TouchableOpacity>
+    )
+   }
     </Animatable.View>
   )
 }
@@ -49,5 +67,18 @@ const Trending = ({posts}) => {
     
   )
 }
-
+const styles = StyleSheet.create({
+  ImageBack: {
+    width: 208, 
+    height: 288, 
+    borderRadius: 33, 
+    marginVertical: 20, 
+    overflow: 'hidden', 
+    shadowColor: 'black', 
+    shadowOffset: { width: 0, height: 10 }, 
+    shadowOpacity: 0.4, 
+    shadowRadius: 10, 
+    elevation: 10, 
+  },
+});
 export default Trending
