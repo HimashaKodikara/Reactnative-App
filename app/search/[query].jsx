e818c87da76b0c3ec0954f23c5a14eee4252b4bb
19840769 <kodikarah.21@uom.lk>
2024-08-28 23:45:42 +0530

    updated app write

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Search = () => {
   const {query} = useLocalSearchParams();
   return (
-    <View>
+    <SafeAreaView style={{backgroundColor:'black', height:'100%'}}>
       <Text style={{color:'white',fontSize:35}}>{query}</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
