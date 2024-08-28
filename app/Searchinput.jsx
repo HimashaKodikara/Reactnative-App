92b7356875e5e811fae3f8d49b9e5f59886ec291
19840769 <kodikarah.21@uom.lk>
2024-08-28 23:27:55 +0530

    updated

--- app/Searchinput.jsx
+++ app/Searchinput.jsx
@@ -2,22 +2,29 @@ import { View, Text, StyleSheet, TextInput, TouchableOpacity,Image } from 'react
 import React, { useState } from 'react';
 
 import {icons} from '../constants'
-import { usePathname } from 'expo-router';
-const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
+import { router,usePathname } from 'expo-router';
+const SearchInput = () => {
   const pathname = usePathname()
-  const [query,setQuery] = useState('');
+  const [query,setQuery] = useState("");
   return (
     
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
-          value={value}
+          value={query}
           placeholder = "Search for a video topic"
           placeholderTextColor="white"
-          onChangeText={handleChangeText}
-          secureTextEntry ={title === 'Password' && !showpassword }
+          onChangeText={(e) => setQuery(e)}
+        
         />
-       <TouchableOpacity>
+       <TouchableOpacity
+       onPress={() =>{
+        if(!query){
+          return Alert.alert('Missing query , Please input something to search results across database ')
+        }
+        if(pathname.startsWith('/search')) router.setParams({query});
+          else router.push(`/search/${query}`)
+       }}>
            
            <Image source={icons.search}style={{width:25,height:25,marginTop:7}}/>
        </TouchableOpacity>
