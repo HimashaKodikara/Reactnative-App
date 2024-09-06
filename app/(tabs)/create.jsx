210b0046f77ddd502d6a73ca4a8bbf2cd8ab72e0
19840769 <kodikarah.21@uom.lk>
2024-09-06 23:48:38 +0530

    updated

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,8 +1,9 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormFiled from '../../FormFiled';
-import {Video, ResizeMode} from 'expo-av';
+import { Video, ResizeMode } from 'expo-av';
+import { icons } from '../../constants';
 
 const Create = () => {
   const [uploading, setUploading] = useState(false);
@@ -29,17 +30,42 @@ const Create = () => {
           <Text style={{ color: 'white' }}>Upload Video</Text>
           <TouchableOpacity>
             {form.video ? (
-              <Video/>
-            ):(
-              <View style={{widht:'100%', height:'40px',paddingVertical:'4px',
-                backgroundColor:'black', rounded:'2xl' ,justifyContent:'center',
-                alignItems:'center' 
+              <Video 
+              source={{uri: form.video.uri}}
+              style={{width:'100%',height:'64px', borderRadius:'16',}}
+              useNativeControls
+              resizeMode={ResizeMode.COVER}
+              isLooping/>
+            ) : (
+              <View style={{
+                width: '100%', // Equivalent to Tailwind's w-full
+                height: 160, // Equivalent to Tailwind's h-40 (40 * 4 = 160px)
+                paddingHorizontal: 16, // Equivalent to Tailwind's px-4 (4 * 4 = 16px)
+                backgroundColor: '#1F2937', // Equivalent to Tailwind's bg-black-100 (use actual color code)
+                borderRadius: 16, // Equivalent to Tailwind's rounded-2xl (2xl usually means 16px)
+                borderWidth: 1, // Equivalent to Tailwind's border
+                borderColor: '#D1D5DB', // Equivalent to Tailwind's border-black-200 (use actual color code)
+                justifyContent: 'center', // Equivalent to Tailwind's justify-center
+                alignItems: 'center',
               }}>
-           <View>
-            
-           </View>
+                <View style={{
+                  width: 56,
+                  height: 56,
+                  borderWidth: 1,
+                  borderStyle: 'dashed',
+                  borderColor: '#d1d5db',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <Image source={icons.upload}
+                    resizeMode='contain'
+                    style={{
+                      width: '50%',
+                      height: '50%',
+                    }} />
+                </View>
               </View>
-            ) }
+            )}
           </TouchableOpacity>
         </View>
       </ScrollView>
