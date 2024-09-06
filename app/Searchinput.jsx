88ae67d09b568d8f3ca814c0d41efe5c915997dd
19840769 <kodikarah.21@uom.lk>
2024-09-06 22:26:10 +0530

    updated

--- app/Searchinput.jsx
+++ app/Searchinput.jsx
@@ -7,20 +7,6 @@ const SearchInput = () => {
   const pathname = usePathname();
   const [query, setQuery] = useState('');
 
-  const handleSearch = () => {
-    if (!query.trim()) {
-      // Display an alert if the input is empty or only has spaces
-      return Alert.alert('Missing Query', 'Please input something to search for results.');
-    }
-
-    // If on the search page, update the query parameter
-    if (pathname.startsWith('/search')) {
-      router.setParams({ query });
-    } else {
-      // Navigate to the search page with the query
-      router.push(`/search/home`);
-    }
-  };
 
   return (
     <View style={styles.inputContainer}>
@@ -31,7 +17,18 @@ const SearchInput = () => {
         placeholderTextColor="white"
         onChangeText={(e) => setQuery(e)}
       />
-      <TouchableOpacity onPress={handleSearch}>
+    <TouchableOpacity
+        onPress={() => {
+          if (query === "")
+            return Alert.alert(
+              "Missing Query",
+              "Please input something to search results across database"
+            );
+
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
         <Image source={icons.search} style={{ width: 25, height: 25, marginTop: 7 }} />
       </TouchableOpacity>
     </View>
