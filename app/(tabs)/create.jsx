cee1e32630c57d11ec423d31e15497b6654b5174
19840769 <kodikarah.21@uom.lk>
2024-09-07 22:01:06 +0530

    uploaded

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -67,6 +67,44 @@ const Create = () => {
               </View>
             )}
           </TouchableOpacity>
+          <View style={{marginTop:7 ,marginBottom :5}}>
+
+          <Text style={{ color: 'white' }}>Thumbnail Image</Text>
+          <TouchableOpacity>
+            {form.thumbnail ? (
+              <Image
+              source ={{uri:form.thumbnail.uri}}
+              resizeMode='cover'
+              style={{ width: '100%',      
+                height: 256,        
+                borderRadius: 16,   
+                backgroundColor: '#ccc'}}
+              />
+            ) : (
+              <View style={{
+                width: '100%',       // Equivalent to w-full
+                height: 64,          // Equivalent to h-16 (16 * 4 = 64 pixels)
+                paddingHorizontal: 16, // Equivalent to px-4
+                backgroundColor: '#f3f4f6', // Replace '#f3f4f6' with your desired color, since bg-black-100 doesn't exist in React Native.
+                borderRadius: 16,    // Equivalent to rounded-2xl
+                borderWidth: 2,      // Equivalent to border-2
+                borderColor: '#d1d5db', // Equivalent to border-black-200 (can adjust for your palette)
+                flexDirection: 'row',// Equivalent to flex-row
+                justifyContent: 'center', // Equivalent to justify-center
+                alignItems: 'center'
+              }}>
+                
+                  <Image source={icons.upload}
+                    resizeMode='contain'
+                    style={{
+                      width: '50%',
+                      height: '50%',
+                    }} />
+                
+              </View>
+            )}
+          </TouchableOpacity>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
